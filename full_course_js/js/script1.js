048e53aae126dd25d0285fa27355ad3962d21efd
hungpt2004 <hungptfpt2004@gmail.com>
2024-04-15 23:21:28 +0700

    WebsiteBoi

---
+++ full_course_js/js/script1.js
@@ -0,0 +1,11 @@
+//------ Dùng để in ra văn bản --------
+// console.log("Hello");
+// console.log("I like Pizaa !"); 
+
+//------ Dùng để in một alert trong trang web -------
+// window.alert("This is an alert");
+// window.alert("I like pizza!");
+
+//------ lấy phần tử theo ID bên HTML ------
+// document.getElementById("myH1").textContent= "Hello";
+// document.getElementById("myP").textContent="I like pizza !";
